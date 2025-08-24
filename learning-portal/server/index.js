281a5be88897b4466e2a5b7754e2f0498a913306
Danilo-droid <comic.danilo@gmail.com>
2025-08-25 01:20:28 +0200

    server configuration

---
+++ learning-portal/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+app.use(express.json());
+app.use(cors());
+
+const dbConfig = require('./config/dbConfig');
+
+const usersRoute = require('./routes/usersRoute');
+const examsRoute = require('./routes/examsRoute');
+const reportsRoute = require('./routes/reportsRoute');
+
+app.use('/api/users', usersRoute);
+app.use('/api/exams', examsRoute);
+app.use('/api/reports', reportsRoute);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
