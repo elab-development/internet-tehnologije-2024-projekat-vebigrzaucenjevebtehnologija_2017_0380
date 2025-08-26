da29b8a7c47b6003510943f64a54371be2ff8d56
Danilo-droid <comic.danilo@gmail.com>
2025-08-26 11:26:52 +0200

    LoadingSpinner & ProtectedRoute components

--- learning-portal/client/src/App.js
+++ learning-portal/client/src/App.js
@@ -1,5 +1,94 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Login from './pages/common/login/Login';
+import Register from './pages/common/register/Register';
+import Home from './pages/common/home/Home';
+
+import Exams from './pages/admin/exams/Exams';
+import AddEditExam from './pages/admin/exams/AddEditExam';
+import Exam from './pages/user/exam/Exam';
+import Reports from './pages/user/attempts/Reports';
+import AdminReports from './pages/admin/reports/Reports';
+
+import ProtectedRoute from './components/ProtectedRoute';
+import LoaderSpinner from './components/LoaderSpinner';
+
 function App() {
-  return <>Learning portal</>;
+  const { loading } = useSelector((state) => state.loader);
+
+  return (
+    <>
+      {loading && <LoaderSpinner />}
+      <BrowserRouter>
+        <Routes>
+          {/* Common Routes */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+
+          {/* User Routes */}
+          <Route
+            path='/'
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/user/write-exam/:id'
+            element={
+              <ProtectedRoute>
+                <Exam />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/user/reports'
+            element={
+              <ProtectedRoute>
+                <Reports />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Admin Routes */}
+          <Route
+            path='/admin/exams'
+            element={
+              <ProtectedRoute>
+                <Exams />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/admin/exams/add'
+            element={
+              <ProtectedRoute>
+                <AddEditExam />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/admin/exams/edit/:id'
+            element={
+              <ProtectedRoute>
+                <AddEditExam />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path='/admin/reports'
+            element={
+              <ProtectedRoute>
+                <AdminReports />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ learning-portal/client/src/components/LoaderSpinner.js
@@ -0,0 +1,9 @@
+const LoaderSpinner = () => {
+  return (
+    <div className='loader-parent'>
+      <div className='loader'></div>
+    </div>
+  );
+};
+
+export default LoaderSpinner;
