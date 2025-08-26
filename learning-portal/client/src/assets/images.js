9c3ab4a71e3f40c70fb56b1a3cdde4665a57aca0
Danilo-droid <comic.danilo@gmail.com>
2025-08-26 13:36:18 +0200

    category and favicon images

---
+++ learning-portal/client/src/assets/images.js
@@ -0,0 +1,17 @@
+import html from './html.png';
+import css from './css.png';
+import js from './js.png';
+import node from './node.png';
+import python from './python.png';
+import php from './php.png';
+import react from './react.png';
+
+export default {
+  html,
+  css,
+  js,
+  node,
+  python,
+  php,
+  react,
+};
